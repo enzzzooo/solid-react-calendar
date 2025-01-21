4d72eede19cd0d066b02a1419a16999b0999953d
Enzo <enzzzooo@proton.me>
2025-01-21 03:12:27 +0000

    publish

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,5 +1,4 @@
 "use client";
-import "./main.css";
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import { format, startOfDay } from "date-fns";
 import Calendar from "./Calendar";
